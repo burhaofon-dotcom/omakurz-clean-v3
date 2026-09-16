e8234441cb3188b4b7f1d54b4bc5559572095dc4
burhaofon-dotcom <burhaofon@gmail.com>
2026-09-16 22:28:50 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ generation_config = {
 }
 
 model = genai.GenerativeModel(
-    model_name="gemini-1.5-flash",
+  model_name="models/gemini-1.5-flash",
     generation_config=generation_config,
 )
 
